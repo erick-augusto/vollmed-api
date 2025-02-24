b0218c18334dbaaf7611236e39817573fadd3202
erick-augusto <erick_aug@hotmail.com>
2025-02-24 19:31:30 -0300

    Criando validações para o cadastro de consultas

--- src/main/java/med/voll/api/domain/paciente/PacienteRepository.java
+++ src/main/java/med/voll/api/domain/paciente/PacienteRepository.java
@@ -1,7 +1,18 @@
 package med.voll.api.domain.paciente;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import jakarta.validation.constraints.NotNull;
 
 public interface PacienteRepository extends JpaRepository<Paciente, Long> {
+
+    @Query("""
+            select p.ativo
+            from Paciente p
+            where
+            p.id = :id
+            """)
+    Boolean findAtivoById(Long idPaciente);
 
 }
